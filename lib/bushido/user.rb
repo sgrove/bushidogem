6628932e015b3878d38e19d1676a28d3d7642359
Sean Grove <s@bushi.do>
2011-05-27 10:11:33 -0700

    Documenting the User methods and a bit of style refactoring

--- lib/bushido/user.rb
+++ lib/bushido/user.rb
@@ -1,23 +1,29 @@
 module Bushido
+  # Bushido User enables user validation against Bushido's server
   class User
     class << self
-      def connect_url
+      def unity_url #:nodoc:
         "#{Bushido::Platform.host}/connect/v1"
       end
-            
+
+
+      # Checkswhether user an email and password correspond to a valid bushido
+      # user. Returns nil if false, or the Bushido user's ID if true.
       def valid?(email, pass)
         params = {}
         params[:email] = email
         params[:pass] = pass
-        Bushido::Command.post_command(connect_url+"/valid", params)
+        Bushido::Command.post_command("#{unity_url}/valid", params)
       end
       
+
+      # Checks whether email corresponds to a valid Bushido user.
+      # Returns true or false
       def exists?(email)
         params = {}
         params[:email] = email
-        Bushido::Command.post_command(connect_url+"/exists", params)
+        Bushido::Command.post_command("#{unity_url}/exists", params)
       end
-
     end
   end
 end
