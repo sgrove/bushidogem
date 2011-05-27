01a23005ff62f50843d36d3d342d68f667c9782c
Sean Grove <s@bushi.do>
2011-05-27 12:59:21 -0700

    Cleaner event interface

--- lib/bushido.rb
+++ lib/bushido.rb
@@ -1,15 +1,13 @@
 module Bushido #:nodoc:
-  require 'rubygems'
   require 'optparse'
   require 'rest-client'
   require 'json'
   require 'highline/import'
-  require 'yaml'
 
   require "bushido/platform"
   require "bushido/utils"
   require "bushido/command"
   require "bushido/app"
   require "bushido/user"
-  require "bushido/events"
+  require "bushido/event"
 end
--- lib/bushido/event.rb
+++ lib/bushido/event.rb
@@ -7,26 +7,34 @@ module Bushido
   # Data will hold the arbitrary data for the type of event signalled
   class Event
     begin
-      @@events = YAML.load(ENV["BUSHIDO_EVENTS"]) #:nodoc:
+      @@events = JSON.parse(ENV["BUSHIDO_EVENTS"]) #:nodoc:
     rescue
       @@events = []
     end
 
+    attr_reader :category, :name, :data
+
     class << self
       # Lists all events
       def all
-        @@events
+        @@events.collect{ |e| Event.new(e) }
       end
 
       # Lists the first (oldest) event
       def first
-        @@events.first
+        Event.new(@@events.first)
       end
 
       # Lists the last (newest) event
       def last
-        @@events.last
+        Event.new(@@events.last)
       end
+    end
+
+    def initialize(options={})
+      @category = options["category"]
+      @name = options["name"]
+      @data = options["data"]
     end
   end
 end
