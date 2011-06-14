60175dce60450244928bd5e831d3eb66e85d2122
kevzettler <kevzettler@gmail.com>
2011-06-14 13:33:28 -0700

    left bad class methods in data

--- lib/bushido/data.rb
+++ lib/bushido/data.rb
@@ -3,31 +3,7 @@ module Bushido
     
     extend Hooks
 
-    class << self
-      def fire data, *hooks
-        unless @@hooks[:global].nil?
-          @@hooks[:global].call('global', data)
-        end
-        
-        if hooks.length > 0
-          hooks.each do |h|          
-            unless @@hooks[h].nil? 
-              @@hooks[h].call(h, data)
-            end
-          end
-        end
-      end
-      
-      def listen *hooks, &block
-        if hooks.empty? and block_given?
-          @@hooks[:global] = block
-        elsif !hooks.nil? and block_given?
-          hooks.each do |h|
-            @@hooks[h] = block
-          end
-        end
-      end
-      
+    class << self      
       # POST /apps/:id/bus
       def publish(model, model_data)
         data = {}
