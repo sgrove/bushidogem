bcb9c796cf974c7302738f7e48247466b6172a8c
kevzettler <kevzettler@gmail.com>
2011-06-17 13:13:57 -0700

    fixed syntax on data::fire params

--- lib/bushido/envs.rb
+++ lib/bushido/envs.rb
@@ -4,20 +4,20 @@ module Bushido
     @@hooks = {}
     
     class << self
-      def fire *hooks
+      def fire(data, *hooks)
         unless @@hooks[:global].nil?
-          @@hooks[:global].call('global')
+          @@hooks[:global].call(data, 'global')
         end
-        
+
         if hooks.length > 0
           hooks.each do |h|          
             unless @@hooks[h].nil? 
-              @@hooks[h].call(h, data)
+              @@hooks[h].call(data, hook)
             end
           end
         end
       end
-      
+
       def listen *hooks, &block
         if hooks.empty? and block_given?
           @@hooks[:global] = block
