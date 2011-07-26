cb4b43a3ae2fef3fce90249f4f9aad2b056a5f9b
Sean Grove <s@bushi.do>
2011-07-26 16:46:55 -0700

    First bits of the bushido gem spec tests

--- Gemfile
+++ Gemfile
@@ -4,17 +4,17 @@ source "http://rubygems.org"
 # Specify your gem's dependencies in bushido.gemspec
 gemspec
 
-
 gem "rails", "~> 3.0.7"
 gem "sqlite3-ruby"
 
 group :test do
   # gem "webrat", "0.7.2", :require => false
-  #   gem "mocha", :require => false
+  # gem "mocha", :require => false
+  gem 'rspec'
 end
 
 platforms :ruby do
-  #gem "sqlite3-ruby"
+  # gem "sqlite3-ruby"
 
   # group :mongoid do
   #   gem "mongo", "~> 1.3.0"
--- app/controllers/bushido/envs_controller.rb
+++ app/controllers/bushido/envs_controller.rb
@@ -6,22 +6,23 @@ module Bushido
         respond_to do |format|
           format.html { render :layout => false, :text => true, :status => :forbidden }
           format.json { render :status => :unprocessable_entity }
-          return
         end
-      end
 
-      ENV[params[:id]] = params[:value]
-      @value = ENV[params[:id]]
-      
-      respond_to do |format|
-        if @value != ENV[params[:id]]
-          format.html{render :layout => false, :text => true, :status => :unprocessable_entity}
-          format.json{render :status => :unprocessable_entity}
-        else
-          puts "Firing update hooks method from controller"
-          Bushido::Hooks.fire(params[:id], {params[:id] => ENV[params[:id]]})
-          format.html{render :text => true}
-          format.json{render :json => {params[:id] => ENV[params[:id]]}}
+      else
+
+        ENV[params[:id]] = params[:value]
+        @value = ENV[params[:id]]
+        
+        respond_to do |format|
+          if @value != ENV[params[:id]]
+            format.html{render :layout => false, :text => true, :status => :unprocessable_entity}
+            format.json{render :status => :unprocessable_entity}
+          else
+            puts "Firing update hooks method from controller"
+            Bushido::Hooks.fire(params[:id], {params[:id] => ENV[params[:id]]})
+            format.html{render :text => true}
+            format.json{render :json => {params[:id] => ENV[params[:id]]}}
+          end
         end
       end
     end
