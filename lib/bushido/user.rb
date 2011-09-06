bc8908bebf46611a11ee68b80d8d3ecd541fb4d0
Akash Manohar J <akash@akash.im>
2011-09-06 22:41:48 +0530

    added invite API
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/bushido/user.rb
+++ lib/bushido/user.rb
@@ -6,7 +6,6 @@ module Bushido
         "#{Bushido::Platform.host}/unity/v1"
       end
 
-
       # Checks whether user an email and password correspond to a valid bushido
       # user. Returns nil if false, or the Bushido user's ID if true.
       def valid?(email, pass)
@@ -23,6 +22,22 @@ module Bushido
         params = {}
         params[:email] = email
         Bushido::Command.post_command("#{unity_url}/exists", params)
+      end
+
+      # Use this to invite a user to an application on Bushido.
+      # Bushido::User.invite("jake@navi-on-pandora.com")
+      def invite(email)
+        if exists?
+            # TODO add an invite to the db and send an email if the user has email notifications enabled
+        else
+            # TODO send a Bushido invite email with short description of the app and also a box of chocolates
+        end
+      end
+
+      # List all pending invites
+      # Bushido::User.pending_invites
+      def pending_invites
+          # TODO returns a list of pending invites (email addresses)
       end
     end
   end
