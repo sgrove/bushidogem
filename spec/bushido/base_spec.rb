5856d2a8890ae8829730c2088ed9e866df77921b
Akash Manohar J <akash@akash.im>
2011-09-30 01:00:33 +0530

    now tests url helper methods against full string
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/bushido/base_spec.rb
+++ spec/bushido/base_spec.rb
@@ -13,11 +13,10 @@ describe "Bushido::Base" do
   end
 
   it "should have url helper methods" do
-    
     url_helpers.stringify_keys.each do |prefix, method_names|
-      method_names.each do |m|
-        full_method_name = m.to_s + "_" + prefix + "_" + "url"
-        Bushido::Base.send(full_method_name.to_sym).should be_kind_of(String)
+      method_names.each do |method_name|
+        full_method_name = method_name.to_s + "_" + prefix + "_" + "url"
+        Bushido::Base.send(full_method_name.to_sym).should == "#{Bushido::Platform.host}/#{prefix}/#{Bushido::Config.api_version}/#{method_name}"
       end
     end
   end
