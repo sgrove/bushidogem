240d48293169a5c44e58f556ef0a13d76f8d1297
Akash Manohar J <akash@akash.im>
2011-09-30 08:43:50 +0530

    fixed cover_me by adding cover_me config
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,11 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'bushido'
 
+CoverMe.config do |c|
+    # where is your project's root:
+    c.project.root = File.expand_path('../..', __FILE__)
+end
+
 RSpec.configure do |config|
   # not yet decided on what to configure here
 end
