bead62a359d41393d050020512cc4e6d97d3ffc0
Akash Manohar J <akash@akash.im>
2011-10-11 21:00:00 +0530

    changed post request auth token key to string
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/bushido/command.rb
+++ lib/bushido/command.rb
@@ -27,7 +27,7 @@ module Bushido
 
       def post_command(url, params)
         @@request_count += 1
-        params.merge!({:auth_token => Bushido::Platform.key}) if params[:auth_token].nil? unless Bushido::Platform.key.nil?
+        params.merge!({"auth_token" => Bushido::Platform.key}) if params[:auth_token].nil? unless Bushido::Platform.key.nil?
 
         begin
           raw = RestClient.post(url, params.to_json, :content_type => :json, :accept => :json)
