94a61071fb00f32bda10e9c056b97afab0352584
Sean Grove <s@bushi.do>
2011-11-15 13:40:10 -0800

    Adds BUSHIDO_PROTOCOL as a variable that can be passed in, cleans up platform.rb, and adds some error handling for action_mailer.rb

--- lib/bushido/action_mailer.rb
+++ lib/bushido/action_mailer.rb
@@ -6,13 +6,19 @@ module ActionMailer
 
       puts result.inspect
 
-      if result["success"] == true
+      if result
+        if result["success"] == true
         Bushido::SMTP.setup_action_mailer_smtp!
 
         unless logger.nil?
           logger.info "App allowed to send email, sending via SMTP"
           __send__("perform_delivery_smtp", mail) if perform_deliveries
         end
+        else
+          logger.info "Unable to send email: #{result['message']}"
+        end
+      else
+        logger.info "Unable to contact Bushido to verify email credentials"
       end
     end
   end
