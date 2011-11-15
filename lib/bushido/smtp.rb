23409b1081fa7b37b65f15edb97dedca474ddf2c
Sean Grove <s@bushi.do>
2011-11-15 14:15:49 -0800

    Completes email threshold checking and ActionMailer configuration for bushidogem

--- lib/bushido/smtp.rb
+++ lib/bushido/smtp.rb
@@ -3,19 +3,20 @@ module Bushido
     class << self
       [:tls, :server, :port, :domain, :authentication, :user, :password].each do |method_name|
         define_method "#{method_name}".to_sym do
-          ENV[method_name.to_s.upcase]
+          ENV["SMTP_#{method_name.to_s.upcase}"]
         end
       end
 
       def setup_action_mailer_smtp!
         ActionMailer::Base.smtp_settings = {
-          :tls =>            Bushido::SMTP.tls,
-          :address =>        Bushido::SMTP.server,
-          :port =>           Bushido::SMTP.port,
-          :domain =>         Bushido::SMTP.domain,
-          :authentication => Bushido::SMTP.authentication,
-          :user_name =>      Bushido::SMTP.user,
-          :password =>       Bushido::SMTP.password,
+          :enable_starttls_auto => Bushido::SMTP.tls,
+          :tls =>                  Bushido::SMTP.tls,
+          :address =>              Bushido::SMTP.server,
+          :port =>                 Bushido::SMTP.port,
+          :domain =>               Bushido::SMTP.domain,
+          :authentication =>       Bushido::SMTP.authentication,
+          :user_name =>            Bushido::SMTP.user,
+          :password =>             Bushido::SMTP.password,
         }
       end
     end
