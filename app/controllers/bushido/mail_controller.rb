2fe4f2491890ece69f166ad00da33f12a3c02ae0
Sean Grove <s@bushi.do>
2011-11-26 14:47:02 -0800

    Updating mail controller format

--- app/controllers/bushido/mail_controller.rb
+++ app/controllers/bushido/mail_controller.rb
@@ -4,31 +4,25 @@ module Bushido
 
     # POST /bushido/mail
     def index
-      hook_data = {}
-      hook_data[:category]     = "mail"
-      hook_data[:event]        = "received"
-      hook_data[:data]         = {}
-      hook_data[:data][:mail]  = {}
+      hook_data             = {}
+      hook_data["category"] = "mail"
+      hook_data["event"]    = "received"
+      hook_data["data"]     = {}
 
-      mailgun_params = [
-        "recipient",
-        "sender",
-        "from",
-        "subject",
-        "body-plain",
-        "stripped-text",
-        "stripped-signature",
-        "body-html",
-        "stripped-html",
-        "attachment-count",
-        "timestamp",
-        "token",
-        "signature"]
+      attachments = []
 
-      mailgun_params.each do |m|
-        hook_data[:data][:mail][m] = params[m]
+      # Strip the attachments first
+      params.keys.each do |key|
+        attachments << params.delete(key) if key =~ /attachment-\d+/
       end
-                                                    
+
+      # Copy the params to the hook data
+      (params.keys - ["controller", "action"]).each do |param|
+        hook_data["data"][param.downcase] = params[param]
+      end
+
+      hook_data["attachments"] = attachments
+
       Bushido::Data.fire(hook_data, "mail.received")
       render :text => "ok", :status => 200
     end
