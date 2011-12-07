59e47748db57c195795e835ca6a054fd9aca12ec
kevzettler <kevzettler@gmail.com>
2011-12-07 11:09:32 -0800

    moved specs around

--- spec/bushido/config_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Bushido::Config" do
-  
-  it "should have API version" do
-    Bushido::Config.api_version.should be_a_kind_of(String)
-  end
-
-end
--- spec/bushido/hooks_spec.rb
+++
@@ -1,5 +0,0 @@
-describe "Hooks" do
-
-  # TODO
-
-end
