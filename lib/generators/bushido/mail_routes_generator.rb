8ae71783f70359a15e1a7e8ea9ffd108a5c127f2
Akash Manohar J <akash@akash.im>
2011-12-16 01:27:57 +0530

    bushido:mail_routes creates email hooks file and an initializer to load it
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/generators/bushido/mail_routes_generator.rb
+++ lib/generators/bushido/mail_routes_generator.rb
@@ -6,7 +6,6 @@ module Bushido
         # Create the lib/bushido directory if it doesnt exist
         Dir.mkdir("#{Rails.root}/lib/bushido") if not Dir.exists? "#{Rails.root}/lib/bushido"
         
-        # Create the mail routes file
         lib "bushido/mail_routes.rb" do
           <<-EOF
 ::Bushido::Mailroute.map do |m|
@@ -18,11 +17,29 @@ module Bushido
 end
           EOF
         end
-        
-        # Create the initializer required to load the mail routes file
-        initializer("bushido_mail_routes.rb", "require './lib/bushido/mail_routes.rb'")
-      end
 
+        lib("bushido/hooks/email_hooks.rb") do
+          <<-EOF
+class BushidoEmailHooks < Bushido::EventObserver
+
+  def mail_simple
+    puts "YAY!"
+    puts params.inspect
+  end
+
+end
+          EOF
+        end
+        
+        initializer "bushido_hooks.rb" do
+          <<-EOF
+Dir["\#{Dir.pwd}/lib/bushido/**/*.rb"].each { |file| require file }
+          EOF
+        end
+        
+        initializer("bushido_mail_routes.rb", "require './lib/bushido/mail_routes.rb'")
+
+      end
     end
   end
 end
