927900b437ee418d48b6066755501f36123f5041
Akash Manohar J <akash@akash.im>
2011-12-16 22:30:42 +0530

    Adds locale and timezone
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/generators/bushido/devise_migration_generator.rb
+++ lib/generators/bushido/devise_migration_generator.rb
@@ -9,6 +9,8 @@ module Bushido
         fields_to_add << "first_name" if not new_resource.respond_to?(:first_name)
         fields_to_add << "last_name"  if not new_resource.respond_to?(:last_name)
         fields_to_add << "email"      if not new_resource.respond_to?(:email)
+        fields_to_add << "locale"     if not new_resource.respond_to?(:locale)
+        fields_to_add << "timezone"   if not new_resource.respond_to?(:timezone)
 
         inject_into_class "app/models/#{class_name.underscore}.rb", class_name, "attr_accessor :ido_id\n"
 
